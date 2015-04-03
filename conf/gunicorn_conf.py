f9706ac9c2829529a4826b5467958e1fae1dd7b9
philmui <thephilmui@gmail.com>
2015-04-03 09:09:51 -0700

    added logging to gunicorn

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -2,3 +2,10 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
+
+# spew = True
+debug = True
+pidfile = /var/run/gunicorn.pid
+loglevel = debug
+accesslog = /var/log/gunicorn_access.log
+errorlog = /var/log/gunicorn_error.log
